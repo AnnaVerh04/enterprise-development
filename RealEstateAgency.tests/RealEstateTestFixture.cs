ee1641c8702ff57cf2a48327527b04b45c6a11ce
AnyaVerkhovaia <anyaverkhovaia@gmail.com>
2025-09-23 20:27:45 +0400

    Adding test data about real estate and requests

--- RealEstateAgency.tests/RealEstateTestFixture.cs
+++ RealEstateAgency.tests/RealEstateTestFixture.cs
@@ -12,6 +12,8 @@ public class RealEstateTestFixture
     public RealEstateTestFixture()
     {
         Counterparties = GenerateCounterparties();
+        Properties = GenerateProperties();
+        Requests = GenerateRequests(Counterparties, Properties);
     }
 
     private List<Counterparty> GenerateCounterparties()
@@ -31,5 +33,314 @@ public class RealEstateTestFixture
             new() { Id = 11, FullName = "Зайцева Наталья Петровна", PassportNumber = "4501 123466", PhoneNumber = "+7-999-111-22-43" },
             new() { Id = 12, FullName = "Белов Игорь Васильевич", PassportNumber = "4501 123467", PhoneNumber = "+7-999-111-22-44" }
         };
+    }
+
+    private List<RealEstateProperty> GenerateProperties()
+    {
+        return new List<RealEstateProperty>
+    {
+        new() {
+            Id = 1,
+            Type = PropertyType.Apartment,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:01:0001001:101",
+            Address = "ул. Тверская, 15, кв. 34",
+            TotalFloors = 9,
+            TotalArea = 75.5,
+            RoomsCount = 3,
+            CeilingHeight = 2.7,
+            Floor = 5,
+            HasEncumbrances = false
+        },
+        new() {
+            Id = 2,
+            Type = PropertyType.Apartment,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:01:0001002:102",
+            Address = "ул. Арбат, 25, кв. 12",
+            TotalFloors = 5,
+            TotalArea = 45.0,
+            RoomsCount = 2,
+            CeilingHeight = 2.5,
+            Floor = 3,
+            HasEncumbrances = true
+        },
+        new() {
+            Id = 3,
+            Type = PropertyType.Apartment,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:01:0001003:103",
+            Address = "пр-т Мира, 10, кв. 78",
+            TotalFloors = 12,
+            TotalArea = 90.0,
+            RoomsCount = 4,
+            CeilingHeight = 2.8,
+            Floor = 8,
+            HasEncumbrances = false
+        },
+
+        new() {
+            Id = 4,
+            Type = PropertyType.House,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:02:0002001:201",
+            Address = "Московская обл., коттеджный поселок 'Лесной', д. 12",
+            TotalFloors = 2,
+            TotalArea = 150.0,
+            RoomsCount = 6,
+            CeilingHeight = 3.0,
+            Floor = null,
+            HasEncumbrances = false
+        },
+        new() {
+            Id = 5,
+            Type = PropertyType.House,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:02:0002002:202",
+            Address = "Московская обл., д. Пушкино, ул. Садовая, 5",
+            TotalFloors = 1,
+            TotalArea = 80.0,
+            RoomsCount = 4,
+            CeilingHeight = 2.6,
+            Floor = null,
+            HasEncumbrances = true
+        },
+
+        new() {
+            Id = 6,
+            Type = PropertyType.Townhouse,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:03:0003001:301",
+            Address = "пос. Рублево, таунхаусный комплекс 'Резиденция', к. 7",
+            TotalFloors = 3,
+            TotalArea = 120.0,
+            RoomsCount = 5,
+            CeilingHeight = 2.7,
+            Floor = null,
+            HasEncumbrances = false
+        },
+        new() {
+            Id = 7,
+            Type = PropertyType.Townhouse,
+            Purpose = PropertyPurpose.Residential,
+            CadastralNumber = "77:03:0003002:302",
+            Address = "пос. Барвиха, таунхаусный комплекс 'Престиж', к. 3",
+            TotalFloors = 2,
+            TotalArea = 95.0,
+            RoomsCount = 4,
+            CeilingHeight = 2.8,
+            Floor = null,
+            HasEncumbrances = false
+        },
+
+        new() {
+            Id = 8,
+            Type = PropertyType.Commercial,
+            Purpose = PropertyPurpose.Commercial,
+            CadastralNumber = "77:05:0005001:501",
+            Address = "ул. Новый Арбат, 15, офис 300",
+            TotalFloors = 10,
+            TotalArea = 60.0,
+            RoomsCount = 2,
+            CeilingHeight = 2.8,
+            Floor = 3,
+            HasEncumbrances = false
+        },
+        new() {
+            Id = 9,
+            Type = PropertyType.Commercial,
+            Purpose = PropertyPurpose.Commercial,
+            CadastralNumber = "77:05:0005002:502",
+            Address = "ул. Тверская-Ямская, 8, магазин",
+            TotalFloors = 3,
+            TotalArea = 85.0,
+            RoomsCount = 1,
+            CeilingHeight = 3.2,
+            Floor = 1,
+            HasEncumbrances = true
+        },
+
+        new() {
+            Id = 10,
+            Type = PropertyType.ParkingSpace,
+            Purpose = PropertyPurpose.Commercial,
+            CadastralNumber = "77:06:0006001:601",
+            Address = "ул. Садовая-Кудринская, 1, подземный паркинг, место А-15",
+            TotalFloors = null,
+            TotalArea = 12.5,
+            RoomsCount = null,
+            CeilingHeight = 2.2,
+            Floor = -1,
+            HasEncumbrances = true
+        },
+        new() {
+            Id = 11,
+            Type = PropertyType.ParkingSpace,
+            Purpose = PropertyPurpose.Commercial,
+            CadastralNumber = "77:06:0006002:602",
+            Address = "ул. Мясницкая, 20, паркинг, место Б-07",
+            TotalFloors = null,
+            TotalArea = 13.0,
+            RoomsCount = null,
+            CeilingHeight = 2.3,
+            Floor = -2,
+            HasEncumbrances = false
+        },
+
+        new() {
+            Id = 12,
+            Type = PropertyType.Warehouse,
+            Purpose = PropertyPurpose.Industrial,
+            CadastralNumber = "77:07:0007001:701",
+            Address = "промзона 'Южные Ворота', складской комплекс №3",
+            TotalFloors = 1,
+            TotalArea = 500.0,
+            RoomsCount = null,
+            CeilingHeight = 6.0,
+            Floor = null,
+            HasEncumbrances = false
+        },
+        new() {
+            Id = 13,
+            Type = PropertyType.Warehouse,
+            Purpose = PropertyPurpose.Industrial,
+            CadastralNumber = "77:07:0007002:702",
+            Address = "промзона 'Северная', склад №5",
+            TotalFloors = 2,
+            TotalArea = 350.0,
+            RoomsCount = null,
+            CeilingHeight = 5.5,
+            Floor = null,
+            HasEncumbrances = true
+        }
+    };
+    }
+    private List<Request> GenerateRequests(List<Counterparty> counterparties, List<RealEstateProperty> properties)
+    {
+        return new List<Request>
+    {
+        new() {
+            Id = 1,
+            Counterparty = counterparties[0], 
+            Property = properties[0],         
+            Type = RequestType.Sale,
+            Amount = 25000000.00m,
+            Date = new DateTime(2024, 1, 15)
+        },
+        new() {
+            Id = 2,
+            Counterparty = counterparties[1], 
+            Property = properties[1],         
+            Type = RequestType.Sale,
+            Amount = 18000000.00m,
+            Date = new DateTime(2024, 2, 20)
+        },
+        new() {
+            Id = 3,
+            Counterparty = counterparties[3], 
+            Property = properties[3],         
+            Type = RequestType.Sale,
+            Amount = 42000000.00m,
+            Date = new DateTime(2024, 3, 10)
+        },
+        new() {
+            Id = 4,
+            Counterparty = counterparties[6], 
+            Property = properties[5],         
+            Type = RequestType.Sale,
+            Amount = 35000000.00m,
+            Date = new DateTime(2024, 4, 5)
+        },
+        new() {
+            Id = 5,
+            Counterparty = counterparties[8], 
+            Property = properties[7],         
+            Type = RequestType.Sale,
+            Amount = 32000000.00m,
+            Date = new DateTime(2024, 5, 12)
+        },
+        new() {
+            Id = 6,
+            Counterparty = counterparties[10], 
+            Property = properties[9],          
+            Type = RequestType.Sale,
+            Amount = 1500000.00m,
+            Date = new DateTime(2024, 6, 8)
+        },
+        new() {
+            Id = 7,
+            Counterparty = counterparties[11], 
+            Property = properties[11],         
+            Type = RequestType.Sale,
+            Amount = 85000000.00m,
+            Date = new DateTime(2024, 7, 25)
+        },
+
+        new() {
+            Id = 8,
+            Counterparty = counterparties[2],  
+            Property = properties[2],          
+            Type = RequestType.Purchase,
+            Amount = 22000000.00m,
+            Date = new DateTime(2024, 1, 20)
+        },
+        new() {
+            Id = 9,
+            Counterparty = counterparties[4],  
+            Property = properties[4],          
+            Type = RequestType.Purchase,
+            Amount = 15000000.00m,
+            Date = new DateTime(2024, 2, 25)
+        },
+        new() {
+            Id = 10,
+            Counterparty = counterparties[5],  
+            Property = properties[6],          
+            Type = RequestType.Purchase,
+            Amount = 28000000.00m,
+            Date = new DateTime(2024, 3, 15)
+        },
+        new() {
+            Id = 11,
+            Counterparty = counterparties[7],  
+            Property = properties[8],          
+            Type = RequestType.Purchase,
+            Amount = 25000000.00m,
+            Date = new DateTime(2024, 4, 18)
+        },
+        new() {
+            Id = 12,
+            Counterparty = counterparties[9],  
+            Property = properties[10],         
+            Type = RequestType.Purchase,
+            Amount = 1800000.00m,
+            Date = new DateTime(2024, 5, 22)
+        },
+        new() {
+            Id = 13,
+            Counterparty = counterparties[2],  
+            Property = properties[12],         
+            Type = RequestType.Purchase,
+            Amount = 60000000.00m,
+            Date = new DateTime(2024, 6, 30)
+        },
+
+        new() {
+            Id = 14,
+            Counterparty = counterparties[1],  
+            Property = properties[0],          
+            Type = RequestType.Purchase,
+            Amount = 24000000.00m,
+            Date = new DateTime(2024, 8, 10)
+        },
+        new() {
+            Id = 15,
+            Counterparty = counterparties[3],  
+            Property = properties[1],          
+            Type = RequestType.Sale,
+            Amount = 19000000.00m,
+            Date = new DateTime(2024, 9, 5)
+        }
+    };
     }
 }
